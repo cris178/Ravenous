83e4ae5d9e29e75a38a322be5a6146d633414caf
Cristian <cris178@outlook.com>
2019-07-16 21:04:26 -0700

    Latest Ver

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,21 +1,18 @@
-import React from 'react';
-import './BusinessList.css';
-import Business from '../Business/Business';
+import React from "react";
+import "./BusinessList.css";
+import Business from "../Business/Business";
 
-class BusinessList extends React.Component{
-    render(){
-
-        return(
-            <div className="BusinessList">
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-                <Business />
-            </div>
-        );
-    }
+class BusinessList extends React.Component {
+  render() {
+    return (
+      //Here we take the array get the individual objects and map them to a Business component and pass the individual object to the component
+      <div className="BusinessList">
+        {this.props.businesses.map(business => {
+          return <Business business={business} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default BusinessList;
